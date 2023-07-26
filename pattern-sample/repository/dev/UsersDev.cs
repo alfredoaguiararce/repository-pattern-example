9942ad7e6f3f22d61043625b15aed0d0d3c9b828
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-07-26 13:40:29 -0700

    [ADD] Training and dev clasess

---
+++ pattern-sample/repository/dev/UsersDev.cs
@@ -0,0 +1,31 @@
+﻿using pattern_sample.abtractions;
+
+namespace pattern_sample.repository.dev
+{
+    /*
+        Al implmentar la clase abstracta se tiene el contrato-interfaz necesaaria de los metodos que comparten ambos ambientes
+        pero al tratarse de clases diferentes tanto UsersDev como UsersTraining pueden implmeentar estos metodos de manera diferente 
+        mientras estos cumplan con los resultados esperados por el contrato
+     */
+    public class UsersDev : UsersBase
+    {
+
+        public override void metodo_1()
+        {
+            // por ejemplo para dev el codigo del metodo 1 puede ser 
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+        }
+
+        public override void metodo_2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void metodo_3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ pattern-sample/repository/trainning/UsersTraining.cs
@@ -0,0 +1,41 @@
+﻿using pattern_sample.abtractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern_sample.repository.trainning
+{
+    internal class UsersTraining : UsersBase
+    {
+        public override void metodo_1()
+        {
+            // por ejemplo para dev el codigo del metodo 1 puede ser 
+            //int a = 1;
+            //int b = 2;
+            //int c = a + b;
+
+            // Pero para training podria ser
+            int a = 1;
+            int b = 2;
+            int c = a * b;
+
+            // esto no interfiere ya que se cumple con el contrato de la interfaz sin embargo 
+            /*
+                Esto pudiera ser mas complejo
+                como el manejo de dos ambientes donde por ejemplo en dev se usara entity framework y en training se usara Dapper 
+             */
+        }
+
+        public override void metodo_2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void metodo_3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
